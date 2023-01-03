2c7fc8e18882fc79d1988a482da641044a9d24bc
haerimRoh <mtxpvksh@gmail.com>
2023-01-03 22:13:08 +0900

    [feat] DiaryList에 날짜순 정렬기능 추가 <ControlMenu/>

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -18,10 +18,23 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+  const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList)); //원본배열을 건들이지 않기 위해
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <div>
       <ControlMenu value={sortType} onChange={setSortType} />
-      {diaryList.map((it) => (
+      {getProcessedDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
