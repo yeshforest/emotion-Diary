8b4733766e2677fe0beb49f0f11084f8b725a8f3
haerimRoh <mtxpvksh@gmail.com>
2023-01-04 21:17:51 +0900

    [feat] DiaryItem 컴포넌트화 - emotion-img

---
+++ src/components/DiaryItem.js
@@ -0,0 +1,20 @@
+const DiaryItem = ({ id, emotion, content, date }) => {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  return (
+    <div className="DiaryItem">
+      <div
+        className={[
+          "emotion_img_wrapper",
+          `emotion_img_wrapper_${emotion}`,
+        ].join(" ")}
+      >
+        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default DiaryItem;
--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
+import DiaryItem from "./DiaryItem";
 
 const sortOptionList = [
   { value: "latest", name: "최신순" },
@@ -79,7 +80,7 @@ const DiaryList = ({ diaryList }) => {
       </div>
 
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <DiaryItem key={it.id} {...it} />
       ))}
     </div>
   );
