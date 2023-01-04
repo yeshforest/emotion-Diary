ead3e1e1821b7ae1cff41f3e702f0d4f4f4861b9
haerimRoh <mtxpvksh@gmail.com>
2023-01-04 21:27:49 +0900

    [feat] add diary_content in DiaryItem

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,6 +1,8 @@
 const DiaryItem = ({ id, emotion, content, date }) => {
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+
   return (
     <div className="DiaryItem">
       <div
@@ -9,9 +11,15 @@ const DiaryItem = ({ id, emotion, content, date }) => {
           `emotion_img_wrapper_${emotion}`,
         ].join(" ")}
       >
-        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
+        <img
+          src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}
+          alt="사진"
+        />
       </div>
-      <div></div>
+      <div className="info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0, 25)}</div>
+      </div>
       <div></div>
     </div>
   );
