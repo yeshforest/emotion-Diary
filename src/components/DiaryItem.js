b909f2a08297438908d0e7d810c5905d4b323208
haerimRoh <mtxpvksh@gmail.com>
2023-01-04 21:34:08 +0900

    [feat] 수정하기 버튼 추가 In DiaryItem, onClick 이벤트 추가 in DiaryItem

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,11 +1,24 @@
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
+
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
   const strDate = new Date(parseInt(date)).toLocaleDateString();
 
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <div className="DiaryItem">
       <div
+        onClick={goDetail}
         className={[
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
@@ -16,11 +29,13 @@ const DiaryItem = ({ id, emotion, content, date }) => {
           alt="사진"
         />
       </div>
-      <div className="info_wrapper">
+      <div onClick={goDetail} className="info_wrapper">
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
-      <div></div>
+      <div className="btn_wrapper">
+        <MyButton onClick={goEdit} text={"수정하기"} />
+      </div>
     </div>
   );
 };
