831a302aae30fdd4dc382ef21a4d4910fd009770
haerimRoh <mtxpvksh@gmail.com>
2023-01-06 22:21:23 +0900

    [feat] /new 경로의 페이지에 input date기능 추가

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import { useState } from "react";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
 const New = () => {
+  const [date, setData] = useState(getStringDate(new Date()));
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>New</h1>
-      <p>이곳은 일기 작성페이지 입니다.</p>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setData(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
