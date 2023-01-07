65fcaf0da05807bf132813c31810ab5b90c3a140
haerimRoh <mtxpvksh@gmail.com>
2023-01-07 23:26:03 +0900

    [feat] DiaryEditor 날짜선택 기능추가

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,35 +1,10 @@
-import { useNavigate } from "react-router-dom";
-import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-import { useState } from "react";
+import DiaryEditor from "../components/DiaryEditor";
+import Diary from "./Diary";
 
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
 const New = () => {
-  const [date, setData] = useState(getStringDate(new Date()));
-  const navigate = useNavigate();
   return (
     <div>
-      <MyHeader
-        headText={"새 일기쓰기"}
-        leftChild={
-          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
-        }
-      />
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
-            <input
-              className="input-date"
-              value={date}
-              onChange={(e) => setData(e.target.value)}
-              type="date"
-            />
-          </div>
-        </section>
-      </div>
+      <DiaryEditor />
     </div>
   );
 };
